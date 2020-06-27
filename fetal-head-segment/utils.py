ad003290d614578f569dd776f54baff58cdf4bda
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2020-06-27 15:59:21 +0500

    Built utils skeleton

---
+++ fetal-head-segment/utils.py
@@ -0,0 +1,20 @@
+def prepare_for_prediction():
+    pass
+
+def save_image():
+    pass
+
+def dir_exists():
+    pass
+
+def make_dir():
+    pass
+
+def path_exists():
+    pass
+
+def path_type():
+    pass
+
+def yes_no():
+    pass
