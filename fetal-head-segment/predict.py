0cc6a88348ba42d9c091abc2f4d8426558be28b0
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2020-06-28 02:58:25 +0500

    Built predict.py skeleton

---
+++ fetal-head-segment/predict.py
@@ -0,0 +1,18 @@
+class Predict:
+    def __init__(self):
+        pass
+
+    def get_class(self):
+        pass
+
+    def get_segmentation(self):
+        pass
+
+    def process_image(self):
+        pass
+
+    def classification_sequence(self):
+        pass
+
+    def segmentation_sequence(self):
+        pass
