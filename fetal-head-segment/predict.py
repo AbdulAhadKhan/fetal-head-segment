a6cec9e7922754ba91af4769430e09daef3751c6
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2020-06-28 04:19:24 +0500

    Added code for video.py

--- fetal-head-segment/predict.py
+++ fetal-head-segment/predict.py
@@ -2,7 +2,7 @@ from classifier import Classifier
 from segmentor.segmentor import Segmentor
 from utils import prepare_for_prediction
 
-class Predict:
+class Predictor:
     def __init__(self,
                  segmentor_path,
                  segmentor_indims,
@@ -14,17 +14,18 @@ class Predict:
         self.segmentor_indims = segmentor_indims
         self.classifier = Classifier(classifier_path, class_dict_path)
         self.classifier_indims = classifier_indims
+        self.image_class = ''
 
     def get_class(self, image):
         image = prepare_for_prediction(image, self.classifier_indims)
-        return self.classifier.get_class(image)
+        self.image_class = self.classifier.get_class(image)
 
     def get_segmentation(self, image):
         image = prepare_for_prediction(image, self.segmentor_indims)
         return self.segmentor.segment_image(image)
 
     def process_image(self, image):
-        pred_class = self.get_class(image)
-        if pred_class == 'Head':
+        self.get_class(image)
+        if self.image_class == 'Head':
             image = self.get_segmentation(image)
         return image
--- fetal-head-segment/video.py
+++ fetal-head-segment/video.py
@@ -1,11 +1,30 @@
 import cv2
+from predict import Predictor
+from __main__ import __file__
 
 class Video:
-    def __init__(self):
-        pass
+    def __init__(self, video_path, predictor_settings, 
+                 save=False, save_dir=None):
+        self.video = self._load_video(video_path)
+        self.save = save
+        self.save_dir = save_dir
+        self.predictor = Predictor(**predictor_settings)
+
+    @staticmethod
+    def _load_video(video_path):
+        return cv2.VideoCapture(video_path)
 
     def stream(self):
-        pass
+        frame_no = 1
+        fps = self._get_frame_rate()
+        while self.video.isOpened():
+            frame = self.video.read()[1]
+            frame = self.predictor.process_image(frame)
+            if self.predictor.image_class == 'Head' and self.save:
+                cv2.imwrite(f'{self.save_dir}/{frame_no}.png', frame)
+            cv2.imshow(__file__, frame)
+            if cv2.waitKey(fps) and 0xFF is ord('esc'):
+                break
 
-    def load_image(self):
-        pass
+    def _get_frame_rate(self):
+        return self.video.get(cv2.CAP_PROP_FPS)
