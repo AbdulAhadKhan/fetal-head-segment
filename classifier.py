1a29e2d045c999bed209b273a08731dba12205df
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2020-06-27 19:38:09 +0500

    Added code for init and _load_class_dict methods

--- classifier.py
+++ classifier.py
@@ -1,14 +1,21 @@
+import pickle
+
+from keras.models import load_model
+
 class Classifier:
-    def get_model(self):
+    def __init__(self, model_path, class_dict_path):
+        self.model = load_model(model_path)
+        self.class_dict_path = self._load_class_dict(class_dict_path)
+
+    @staticmethod
+    def _load_class_dict(class_dict_path):
+        with open(class_dict_path, 'rb') as file:
+            return pickle.load(file)
+
+    def get_probabilites(self):
         pass
 
     def get_class(self):
-        pass
-
-    def load_class_dict(self):
-        pass
-
-    def get_probabilites(self):
         pass
 
     def prob_to_class(self):
