a56f72a49bf4d9244104ca873c3e2475d7b574d5
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2020-06-27 18:57:57 +0500

    Added classifier.py skeleton

---
+++ classifier.py
@@ -0,0 +1,18 @@
+class Classifier:
+    def get_model(self):
+        pass
+
+    def get_class(self):
+        pass
+
+    def load_class_dict(self):
+        pass
+
+    def get_probabilites(self):
+        pass
+
+    def prob_to_class(self):
+        pass
+
+    def translate(self):
+        pass
