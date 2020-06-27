7ddb86c95c96a65a606dd6b3021d3a242d8e78a6
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2020-06-27 17:37:13 +0500

    Added code for prepare_for_prediction

--- fetal-head-segment/utils.py
+++ fetal-head-segment/utils.py
@@ -1,5 +1,12 @@
-def prepare_for_prediction():
-    pass
+import cv2
+import numpy as np
+
+def prepare_for_prediction(image, dims):
+    image = cv2.resize(image, dims, cv2.INTER_NEAREST)
+    image = cv2.cvtColor(cv2, cv2.COLOR_BGR2GRAY)
+    image = np.expand_dims(image, -1)
+    image = np.expand_dims(image, 0)
+    return image
 
 def save_image():
     pass
