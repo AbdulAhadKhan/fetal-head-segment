88e8cb69bf81b16911e4769f4e7900db8f981915
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2020-06-27 21:20:04 +0500

    Built segmentor skeleton

---
+++ fetal-head-segment/segmentor.py
@@ -0,0 +1,15 @@
+class Segmentor:
+    def __init__(self):
+        pass
+
+    def segment_image(self):
+        pass
+
+    def get_model(self):
+        pass
+
+    def get_mask(self):
+        pass
+
+    def embed_mask(self):
+        pass
