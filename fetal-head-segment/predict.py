6cdd018665db5af305b5b7c72d9c06764dc44a08
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2020-06-28 03:33:51 +0500

    Added code for predict

--- fetal-head-segment/predict.py
+++ fetal-head-segment/predict.py
@@ -1,18 +1,30 @@
+from classifier import Classifier
+from segmentor.segmentor import Segmentor
+from utils import prepare_for_prediction
+
 class Predict:
-    def __init__(self):
-        pass
+    def __init__(self,
+                 segmentor_path,
+                 segmentor_indims,
+                 custom_object,
+                 classifier_path,
+                 classifier_indims,
+                 class_dict_path):
+        self.segmentor = Segmentor(segmentor_path, custom_object)
+        self.segmentor_indims = segmentor_indims
+        self.classifier = Classifier(classifier_path, class_dict_path)
+        self.classifier_indims = classifier_indims
 
-    def get_class(self):
-        pass
+    def get_class(self, image):
+        image = prepare_for_prediction(image, self.classifier_indims)
+        return self.classifier.get_class(image)
 
-    def get_segmentation(self):
-        pass
+    def get_segmentation(self, image):
+        image = prepare_for_prediction(image, self.segmentor_indims)
+        return self.segmentor.segment_image(image)
 
-    def process_image(self):
-        pass
-
-    def classification_sequence(self):
-        pass
-
-    def segmentation_sequence(self):
-        pass
+    def process_image(self, image):
+        pred_class = self.get_class(image)
+        if pred_class == 'Head':
+            image = self.get_segmentation(image)
+        return image
