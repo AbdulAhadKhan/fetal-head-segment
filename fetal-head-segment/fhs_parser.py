406904be2844b5c8d93c322eb4c0eedda4c0b07a
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2020-06-28 20:07:20 +0500

    Parser fix

--- fetal-head-segment/fhs_parser.py
+++ fetal-head-segment/fhs_parser.py
@@ -2,16 +2,18 @@ import argparse
 
 class Parser:
     def __init__(self):
-        self._args = argparse.ArgumentParser()
+        self._arg_parser = argparse.ArgumentParser()
 
-    def initialize_arguments(self):
-        self._args.add_argument('path', type=str, help='Path to video/image file')
-        self._args.add_argument('-i', '--image', action='store_false',
-                                help='Specify this option if input is an image')
-        self._args.add_argument('-o', '--output', type=str, default=None,
-                                help='If used the output will also be saved to specified directory')
-        self._args.add_argument('-s', '--subdir', type=str, default=None,
-                                help='Used with -o, make subdir with specified output directory')
+        self._arg_parser.add_argument('path', type=str, help='Path to video/image file')
+        self._arg_parser.add_argument('-i', '--image', action='store_false',
+                                      help='Specify this option if input is an image')
+        self._arg_parser.add_argument('-o', '--output', type=str, default=None,
+                                      help='If used the output will also be saved to ' +
+                                      'specified directory')
+        self._arg_parser.add_argument('-s', '--subdir', type=str, default=None,
+                                      help='Used with -o, make subdir with specified ' +
+                                      'output directory')
 
     def get_arguments(self):
-        return vars(self._args)
+        args = self._arg_parser.parse_args()
+        return vars(args)
