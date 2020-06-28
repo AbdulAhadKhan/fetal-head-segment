776814614be488522c0a8ee838d4052c04b34b84
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2020-06-28 19:15:18 +0500

    Working on execute

--- fetal-head-segment/command.py
+++ fetal-head-segment/command.py
@@ -1,22 +1,39 @@
+import sys
 import yaml
-from parser import Parser
 
-def _get_mode():
-    pass
+from fhs_parser import Parser
+from utils import path_exists
 
-def _test_arguments():
-    pass
+from image import Image
+
+def _get_save_mode(args):
+    return args['o'] is not None
+
+def _test_arguments(args):
+    if not path_exists(args['path']):
+        print('Input path does not exist')
+        return False
+    if args['o'] is not None and path_exists(args['o']):
+        print('Output path does not exist')
+        return False
+    return True
 
 def _get_configs():
     with open('./config.yaml', 'r') as file:
         return yaml.safe_load(file)
 
-def _mode_sequence():
-    pass
+def _mode_sequence(args, save_mode, config):
+    if args['i']:
+        im_predictor = Image(args['path'],
+                             config)
 
 def execute():
     parser = Parser()
     args = parser.get_arguments()
+
+    if not _test_arguments(args):
+        sys.exit(1)
+
     config = _get_configs()
-    mode = _get_mode(args)
-    _mode_sequence(mode, config)
+    save_mode = _get_save_mode(args)
+    _mode_sequence(args, save_mode, config)
