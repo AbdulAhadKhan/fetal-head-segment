a2f590078577ec3d7ccc0b5654c998e24f6c4f67
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2020-06-28 17:08:14 +0500

    Added code for praser

--- parser.py
+++ parser.py
@@ -2,10 +2,16 @@ import argparse
 
 class Parser:
     def __init__(self):
-        pass
+        self._args = argparse.ArgumentParser()
 
     def initialize_arguments(self):
-        pass
+        self._args.add_argument('path', type=str, help='Path to video/image file')
+        self._args.add_argument('-i', '--image', action='store_false',
+                                help='Specify this option if input is an image')
+        self._args.add_argument('-o', '--output', type=str, default=None,
+                                help='If used the output will also be saved to specified directory')
+        self._args.add_argument('-s', '--subdir', type=str, default=None,
+                                help='Used with -o, make subdir with specified output directory')
 
     def get_arguments(self):
-        pass
+        return dict(self._args)
