02df0c9ae0cf4b51ad7fd407df8a7cceebe32210
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2020-06-29 01:45:10 +0500

    Fixed prepare_for_prediction in utils

--- fetal-head-segment/utils.py
+++ fetal-head-segment/utils.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def prepare_for_prediction(image, dims):
     image = cv2.resize(image, dims, cv2.INTER_NEAREST)
-    image = cv2.cvtColor(cv2, cv2.COLOR_BGR2GRAY)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     image = np.expand_dims(image, -1)
     image = np.expand_dims(image, 0)
     return image
