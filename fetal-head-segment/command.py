5486b535db57653eec0888513494819cbf8a1166
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2020-06-28 17:25:27 +0500

    Built command.py skeleton

---
+++ fetal-head-segment/command.py
@@ -0,0 +1,21 @@
+class Command:
+    def __init__(self):
+        pass
+
+    def _get_arguments(self):
+        pass
+
+    def _get_mode(self):
+        pass
+
+    def _test_arguments(self):
+        pass
+
+    def _get_settings(self):
+        pass
+
+    def _mode_sequence(self):
+        pass
+
+    def execute(self):
+        pass
