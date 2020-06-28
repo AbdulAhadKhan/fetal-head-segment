1d45ef568014d415a50baa06a34f9fa1bb57ef52
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2020-06-28 19:28:52 +0500

    Added all code in command.py

--- fetal-head-segment/command.py
+++ fetal-head-segment/command.py
@@ -3,8 +3,10 @@ import yaml
 
 from fhs_parser import Parser
 from utils import path_exists
+from utils import make_dir
 
 from image import Image
+from video import Video
 
 def _get_save_mode(args):
     return args['o'] is not None
@@ -22,10 +24,19 @@ def _get_configs():
     with open('./config.yaml', 'r') as file:
         return yaml.safe_load(file)
 
-def _mode_sequence(args, save_mode, config):
+def _mode_sequence(args, save_mode, save_dir, config):
     if args['i']:
         im_predictor = Image(args['path'],
-                             config)
+                             config,
+                             save_mode,
+                             save_dir)
+        im_predictor.show()
+    else:
+        vid_predictor = Video(args['path'],
+                              config,
+                              save_mode,
+                              save_dir)
+        vid_predictor.stream()
 
 def execute():
     parser = Parser()
@@ -36,4 +47,9 @@ def execute():
 
     config = _get_configs()
     save_mode = _get_save_mode(args)
-    _mode_sequence(args, save_mode, config)
+
+    save_dir = args['o']
+    if args['s'] is not None:
+        save_dir = make_dir(args['o'], args['s'])
+
+    _mode_sequence(args, save_mode, save_dir, config)
