438b7f7dc220af5d4101aba2b351943589444141
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2020-06-29 02:41:53 +0500

    Bug fixes

--- fetal-head-segment/video.py
+++ fetal-head-segment/video.py
@@ -18,11 +18,13 @@ class Video:
     def stream(self):
         frame_no = 1
         fps = self._get_frame_rate()
+        dims = self._get_dims()
         while self.video.isOpened():
             frame = self.video.read()[1]
             frame = self.predictor.process_image(frame)
             if self.predictor.image_class == 'Head' and self.save:
                 save_image(frame, f'{self.save_dir}/{frame_no}.png')
+            frame = cv2.resize(frame, dims, cv2.INTER_NEAREST)
             cv2.imshow(__file__, frame)
             if cv2.waitKey(fps) & 0xFF is ord('q'):
                 break
@@ -30,3 +32,8 @@ class Video:
     def _get_frame_rate(self):
         fps = self.video.get(cv2.CAP_PROP_FPS)
         return int(fps)
+
+    def _get_dims(self):
+        height = self.video.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        width = self.video.get(cv2.CAP_PROP_FRAME_WIDTH)
+        return int(width), int(height)
