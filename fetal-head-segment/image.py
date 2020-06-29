05d5c1a48c21b94eded5f3419c28a77c43cdfa8d
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2020-06-29 20:12:42 +0500

    Added image class embedding

--- fetal-head-segment/image.py
+++ fetal-head-segment/image.py
@@ -2,6 +2,7 @@ import ntpath
 
 import cv2
 from utils import save_image
+from utils import embed_class
 from predict import Predictor
 from __main__ import __file__
 
@@ -20,7 +21,13 @@ class Image:
 
     def show(self):
         image = self.predictor.process_image(self.image)
+        dims = self._get_dims()
         if self.predictor.image_class == 'Head' and self.save:
             save_image(image, f'{self.save_dir}/{self.fname}.png')
+        image = cv2.resize(image, (dims))
+        image = embed_class(image, self.predictor.image_class)
         cv2.imshow(__file__, image)
         cv2.waitKey()
+
+    def _get_dims(self):
+        return self.image.shape[:2]
